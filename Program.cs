ee16b0d8a5c17d66cc67cae81e5b7573af1d863d
Rusal84 <rusal@bk.ru>
2021-04-12 20:13:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace SheduleEV
+{
+  class Program
+  {
+    
+
+    static void Main(string[] args)
+    {
+      SheduleMasClass shed_mas = new SheduleMasClass();
+      SheduleClass shed = new SheduleClass("Task1", 1);
+      shed_mas.Add(shed);
+      shed.name = "Task2";
+      shed.priority = 3;
+      shed_mas.Add(shed);
+      shed.name = "Task3";
+      shed.priority = 2;
+      shed_mas.Add(shed);
+      shed.name = "Task4";
+      shed.priority = 1;
+      shed_mas.Add(shed);
+      shed.name = "Task9";
+      shed.priority = 3;
+      shed_mas.Add(shed);
+      string json = JsonConvert.SerializeObject(shed_mas);
+      Console.WriteLine(json);
+
+      SheduleMasClass dshed_mas = new SheduleMasClass();
+      dshed_mas = JsonConvert.DeserializeObject<SheduleMasClass>(json);
+      Console.WriteLine(dshed_mas);
+      Console.ReadKey();
+    }
+  }
+}
---
+++ SheduleClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheduleEV
+{
+  [Serializable]
+  public class SheduleClass
+  {
+    public SheduleClass(string name, int priority)
+    {
+      this.name = name;
+      this.priority = priority;
+    }
+
+    public SheduleClass()
+    {
+      this.name = "name";
+      this.priority = 0;
+    }
+
+    public string name { set; get; }
+    public int priority { set; get; }
+
+    public override string ToString()
+    {
+      string str1 = String.Format("name: {0} - priority: {1}", name, priority);
+      return str1;
+    }
+  }
+  public class SheduleMasClass
+  {
+    public List<SheduleClass> shed_mas = new List<SheduleClass>();
+
+    public void Add(SheduleClass _shed)
+    {
+      SheduleClass sh = new SheduleClass();
+      sh.name = _shed.name;
+      sh.priority = _shed.priority;
+      shed_mas.Add(sh);
+    }
+
+    public override string ToString()
+    {
+      string str1 = "";
+      foreach(SheduleClass item in shed_mas)
+      {
+        str1 = str1 + "\n" + item;
+      }
+      return str1;
+    }
+
+  }
+
+}
